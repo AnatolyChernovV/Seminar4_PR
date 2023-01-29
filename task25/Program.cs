7e760be8be2ca8f429edda7c9e65001747ab9a60
Anatoly Chernov <chernv-tlja@rambler.ru>
2023-01-29 23:25:17 +0300

    task25, task 27, task 29 added

--- task25/Program.cs
+++ task25/Program.cs
@@ -3,19 +3,20 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int Exp (int numA, int numB){
-  int result = 1;
-  for(int i=1; i <= numB; i++)
-  {
-    result = result * numA;
-  }
+int Exp(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result = result * numA;
+    }
     return result;
 }
 
-  Console.Write("Введите число A: ");
-  int numA = Convert.ToInt32(Console.ReadLine());
-  Console.Write("Введите число B: ");
-  int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
 
-  int exp = Exp (numA, numB);
-  Console.WriteLine("Ответ: " + exp);
+int exp = Exp(numA, numB);
+Console.WriteLine("Ответ: " + exp);
--- task27/Program.cs
+++ task27/Program.cs
@@ -6,21 +6,21 @@
 Console.Write("Введите число N: ");
 int numN = Convert.ToInt32(Console.ReadLine());
 
-  int SumNum (int numN)
-  {
-    
+int SumNum(int numN)
+{
+
     int counter = Convert.ToString(numN).Length;
     int advance = 0;
     int result = 0;
 
     for (int i = 0; i < counter; i++)
     {
-      advance = numN - numN % 10;
-      result = result + (numN - advance);
-      numN = numN / 10;
+        advance = numN - numN % 10;
+        result = result + (numN - advance);
+        numN = numN / 10;
     }
-   return result;
-  }
+    return result;
+}
 
-int sumNum = SumNum (numN);
+int sumNum = SumNum(numN);
 Console.WriteLine("Сумма цифр в числе: " + sumNum);
